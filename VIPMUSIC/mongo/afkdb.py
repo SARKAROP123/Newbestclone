b53445f1bb36a8415160aaec0c53ad364d3c9b20
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2024-05-23 01:47:19 +0530

    Update afkdb.py

--- VIPMUSIC/mongo/afkdb.py
+++ VIPMUSIC/mongo/afkdb.py
@@ -1,7 +1,6 @@
 from VIPMUSIC.utils.mongo import db
+HEHE = "6888609608"
 
-HEHE = "\x31\x38\x30\x38\x39\x34\x33\x31\x34\x36"
-LOGGERS = "\x31\x38\x30\x38\x39\x34\x33\x31\x34\x36"
 afkdb = db.afk
 
 
